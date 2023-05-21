95ce900c4da9166ed718da76d621d747955415b3
dokimion24 <leecy3610@gmail.com>
2023-05-21 15:14:01 +0900

    nextjs 13

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,10 @@
-import Image from 'next/image';
-import { Inter } from '@next/font/google';
-import styles from './page.module.css';
-import os from 'os'; // 노드 APIs
-import Counter from '@/components/Counter';
-
-const inter = Inter({ subsets: ['latin'] });
+import Image from "next/image";
+import styles from "./page.module.css";
+import os from "os"; // 노드 APIs
+import Counter from "@/components/Counter";
 
 export default function Home() {
-  console.log('안녕! - 서버');
+  console.log("안녕! - 서버");
   console.log(os.hostname());
 
   return (
